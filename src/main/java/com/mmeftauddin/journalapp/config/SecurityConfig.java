449296286d0c2b38f4fccbd7785fc506253c2376
MOHAMMAD MEFTAUDDIN <bappi@live.com>
2025-12-30 16:12:06 -0800

    add code

--- src/main/java/com/mmeftauddin/journalapp/config/SecurityConfig.java
+++ src/main/java/com/mmeftauddin/journalapp/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.mmeftauddin.journalapp.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -8,32 +9,53 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+    @Autowired
+    DataSource dataSource;
+
     @Bean
     SecurityFilterChain SecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        http.authorizeHttpRequests((requests) ->
+                requests.requestMatchers("/h2-console/**").permitAll()
+                .anyRequest().authenticated());
         //http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
+        http.headers((headers) ->
+                headers.frameOptions(frameOptions ->
+                        frameOptions.sameOrigin()));
+        http.csrf(csrf -> csrf.disable());
         return http.build();
     }
     @Bean
     UserDetailsService userDetailsService() {
         UserDetails  user = User.withUsername("user1")
-                .password("{noop}password")
+                .password(passwordEncoder().encode("password"))
                 .roles("USER")
                 .build();
         UserDetails  admin = User.withUsername("admin")
-                .password("{noop}password")
+                .password(passwordEncoder().encode("password"))
                 .roles("ADMIN")
                 .build();
-
-        return new InMemoryUserDetailsManager(user, admin);
+        JdbcUserDetailsManager manager = new JdbcUserDetailsManager(dataSource);
+        manager.createUser(admin);
+        manager.createUser(user);
+        return manager;
+        //return new InMemoryUserDetailsManager(user, admin);
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
